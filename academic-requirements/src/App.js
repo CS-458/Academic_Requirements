8dc6d4551775201182508bc920e8e1638b36a2e0
ruudkas <kassidyruud@gmail.com>
2022-10-17 15:04:51 -0500

    Formatting

--- academic-requirements/src/App.js
+++ academic-requirements/src/App.js
@@ -4,20 +4,34 @@ function App() {
   return (
     <div className="App">
       <header className="Four-Year-Plan">
-        <div>
-          <h1>Enter User Input Here</h1>
-          <div class="row">
-            <div class="column" ><button>Major dropdown</button></div>
-            <div class="column" ><button>Concentration dropdown</button></div>
-            <div class="column" ><button>Completed Courses Class</button><button>Completed Courses Number</button></div>
-          </div>
-          <div class="row">
-            <div class="buttoncolumn"> Import previous schedule</div>
-            <div class="buttoncolumn">Continue button</div>
-            <div class="buttoncolumn">Completed Course list</div>
+        <h1>Enter User Input Here</h1>
+      </header>
+      <div class="screen">
+        <div class="row">
+          <div class="column" ><button>Major dropdown</button></div>
+          <div class="column" ><button>Concentration dropdown</button></div>
+          <div class="column" ><button>Completed Class</button><button>Completed Number</button></div>
+        </div>
+        <div class="row2">
+          <div class="column2"><button>Import Schedule</button></div>
+          <div class="column2"><button>Generate My Schedule</button></div>
+          <div class="column2">
+            <center><table>
+              <tr><th><h2>Completed Courses</h2></th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>A random long string of text for testing purposes</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+            </table></center>
           </div>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
