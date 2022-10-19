18e919b800dc5c1af33d25b65033ecac73014bf3
ruudkas <90857751+ruudkas@users.noreply.github.com>
2022-10-19 12:33:36 -0500

    Kassidy branch (#6)
    * Beginning formatting
    
    * Formatting

--- academic-requirements/src/App.js
+++ academic-requirements/src/App.js
@@ -1,23 +1,37 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Four-Year-Plan">
+        <h1>Enter User Input Here</h1>
       </header>
+      <div class="screen">
+        <div class="row">
+          <div class="column" ><button>Major dropdown</button></div>
+          <div class="column" ><button>Concentration dropdown</button></div>
+          <div class="column" ><button>Completed Class</button><button>Completed Number</button></div>
+        </div>
+        <div class="row2">
+          <div class="column2"><button>Import Schedule</button></div>
+          <div class="column2"><button>Generate My Schedule</button></div>
+          <div class="column2">
+            <center><table>
+              <tr><th><h2>Completed Courses</h2></th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>A random long string of text for testing purposes</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+              <tr><th>-</th></tr>
+            </table></center>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
