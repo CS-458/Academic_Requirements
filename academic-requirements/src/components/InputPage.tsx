60475b79d5b9b19bc37ae5f8ab090bc8c8f43450
Tyler Pamperin <pamperint8644@my.uwstout.edu>
2022-10-20 14:05:48 -0500

    Moved input page from app into a specific component

---
+++ academic-requirements/src/components/InputPage.tsx
@@ -0,0 +1,143 @@
+// @ts-nocheck
+import React, {useState, useRef} from 'react';
+import SearchableDropdown from './SearchableDropdown.tsx';
+
+// Temporary imports until we get the real data
+import majors from '../mockDataLists/majors.tsx';
+import concentrations from '../mockDataLists/concentrations.tsx';
+import courseSubjectAcronym from '../mockDataLists/courseSubjectAcronym.tsx';
+import courseNumber from '../mockDataLists/courseNumber.tsx';
+
+const completedClasses : string[] = [];
+
+const InputPage = (props: { showing: boolean} ) => {
+
+  /*
+   General variables
+  */
+  const [major, setMajor] = useState('');
+  //const [minor, setMinor] = useState('');
+  const [concentration, setConcentration] = useState('');
+  const [showConcentration, setShowConcentration] = useState(false);
+  const [concentrationOptions, setConcentrationOptions] = useState<Array<string>>();
+  const [coursesTaken, setCoursesTaken] = useState(null);
+
+  /* 
+    Methods that assign major, minor, or concentration when picking option from a dropdown
+  */
+  function selectedMajor(_major) { 
+    setMajor(_major); 
+    setShowConcentration(true);
+
+    // TODO run a query to update the concentrations when major is selected?
+    setConcentrationOptions(concentrations);
+  }
+
+  //function selectedMinor(_minor) { setMinor(_minor); }
+
+  function selectedConcentration(_concentration) { setConcentration(_concentration); }
+
+  /*
+   Methods for updating the table of previously taken courses
+  */
+  const [selectedAcronym, setSelectedAcronym] = useState('');
+  const [selectedNumber, setSelectedNumber] = useState(0);
+  
+  function selectedCourseSubjectAcronym (_selectedAcronym) {
+    setSelectedAcronym(_selectedAcronym);
+    // TODO update list of course numbers based on the acronym
+  }
+
+  function selectedCourseNumber(_selectedNumber) {
+    setSelectedNumber(_selectedNumber);
+    // TODO update list of courses based on the selected course acronym and number
+
+  }
+
+  const tableRef = useRef<HTMLTableElement>(null);
+  
+  function processCompletedCourse() {
+    /*Check that both dropdowns are filled out*/
+    if (selectedNumber != null && selectedAcronym != null) {
+      /*Add the course to the completed course list*/
+      var arrayLength = completedClasses.push(selectedAcronym + "-" + selectedNumber);
+      /*Output the course into the completed course list*/
+      if (tableRef.current != null) {
+        if (arrayLength > 10) {
+          var location = (arrayLength % 10) - 1;
+          if (location == -1) {
+            location = 9;
+          }
+          var row = document.getElementById("completedCourseTable").rows[
+            location
+          ];
+      } else {
+        var row = tableRef.current.insertRow();
+      }
+      row.insertCell().innerHTML = selectedAcronym+"-"+selectedNumber;
+    }
+    } else {
+      /* TODO alert the user that they need to enter a complete, valid, course*/
+    }
+  }
+  return (
+    <div className="App">
+      <header className="Four-Year-Plan">
+        <h1>Academic Planner</h1>
+      </header>
+      <div className="screen">
+        <div className="row">
+          <div className="column" >
+            <SearchableDropdown 
+              options={majors} 
+              label="Major"
+              onSelectOption={selectedMajor}
+              showDropdown={true}
+              thin={false}
+            />
+          </div>
+          <div className="column">
+            <SearchableDropdown 
+              options={concentrations} 
+              label="Concentration"
+              onSelectOption={selectedConcentration}
+              showDropdown={showConcentration}
+              thin={false}
+            />
+          </div>
+          <div className="courseInput">
+            <SearchableDropdown 
+              options={courseSubjectAcronym} 
+              label="Course Subject"
+              onSelectOption={selectedCourseSubjectAcronym}
+              showDropdown={true}
+              thin={true}
+            />
+              <SearchableDropdown 
+                options={courseNumber} 
+                label="Course Number"
+                onSelectOption={selectedCourseNumber}
+                showDropdown={true}
+                thin={true}
+              />
+          </div>
+        </div>
+        </div>
+        <div className="row2">
+        <button onClick={processCompletedCourse}>Add Course</button>
+          <div className="column2"><button>Import Schedule</button></div>
+          <div className="column2"><button>Generate My Schedule</button></div>
+          <div className="column2">
+          <div className="completedCourses">
+          <h2>Completed Courses</h2>
+            <table id="completedCourseTable" ref={tableRef}>
+              <tbody></tbody>
+            </table>
+          </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default InputPage;
--- academic-requirements/src/mockDataLists/courseNumber.tsx
+++ academic-requirements/src/mockDataLists/courseNumber.tsx
@@ -1,5 +1,5 @@
 const courseNumber = [
-    101, 102, 300, 301, 410, 430
+    '101', '102', '300', '301', '410', '430'
 ]
 
 export default courseNumber;
