b8ad6ada10c596a1f5d3e4e4874296ad648cc6b3
kreklb <98837415+kreklb@users.noreply.github.com>
2022-10-26 11:32:21 -0500

    New things added

--- academic-requirements/src/App.js
+++ academic-requirements/src/App.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import InputPage from "./components/InputPage.tsx";
 import FourYearPlanPage from "./components/FourYearPlanPage.tsx";
 
+
+
 function App() {
+  const [majorData, setMajorData] = useState([]);
+  const [concentrationData, setConcentrationData] = useState([]);
+  const [majorCourseData, setMajorCourseData] = useState([]);
+  const [concentrationCourseData, setConcentrationCourseData] = useState([]);
+
   /* Variables to store necessary info */
   const [clickedGenerate, setClickedGenerate] = useState();
   const [previouslyTakenCourses, setPreviouslyTakenCourses] = useState();
-  const [major, setMajor] = useState("");
-  const [concentration, setConcentration] = useState("");
 
   // Processes when the user clicks to generate the schedule
   function generateSchedule(major, concentration, previousCourses) {
@@ -25,8 +30,43 @@ function App() {
       console.log(course + ", ");
     });
 
+
     //TODO run query with major and concentration
   }
+
+    useEffect(() => {
+      fetch("/major")
+      .then(res => res.json())
+      .then((result) => {
+        setMajorData(result)
+      })
+    }, [])
+
+    useEffect(() => {
+        fetch(`/concentration?majid=${major}`)
+      .then(res => res.json())
+      .then((result) => {
+        setConcentrationData(result)
+      })
+    }, [major])
+
+    useEffect(() => {
+      fetch(`/courses/major?majid=${major}`)
+    .then(res => res.json())
+    .then((result) => {
+      setMajorCourseData(result)
+    })
+  }, [major])
+
+    useEffect(() => {
+      fetch(`/courses/concentration?conid=${concentration}`)
+    .then(res => res.json())
+    .then((result) => {
+      setConcentrationCourseData(result)
+    })
+  }, [concentration])
+
+
 
   return (
     <div>
