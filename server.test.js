713d6116bed125bf7ea687dd2ce2dd284d01e966
kreklb <98837415+kreklb@users.noreply.github.com>
2022-11-07 16:51:48 -0600

    forgot to run precommit
    :anguished:

--- server.test.js
+++ server.test.js
@@ -1,54 +1,58 @@
 const app = require("./server.js");
 const mysql = require("mysql");
-const supertest = require("supertest")
+const supertest = require("supertest");
 const dbconfig = {
-    host: "mscsdb.uwstout.edu",
-    user: "academicSelect",
-    password: "mergeSort45!",
-    database: "academicrequirements",
+  host: "mscsdb.uwstout.edu",
+  user: "academicSelect",
+  password: "mergeSort45!",
+  database: "academicrequirements",
 };
 let connection;
 
 beforeEach(async () => {
-    connection = mysql.createConnection(dbconfig);
+  connection = mysql.createConnection(dbconfig);
 });
 
 afterEach(async () => {
-    connection.end();
+  connection.end();
 });
 
-it('test /major endpoint', async () => {
-    await supertest(app).get("/major")
+it("test /major endpoint", async () => {
+  await supertest(app)
+    .get("/major")
     .expect(200)
     .then((response) => {
-        expect(Array.isArray(response.body)).toBeTruthy();
-        expect(response.body.length).toBeGreaterThan(0);
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toBeGreaterThan(0);
     });
 });
 
-it('test /concentration endpoint', async() => {
-    await supertest(app).get("/concentration?majid=2")
+it("test /concentration endpoint", async () => {
+  await supertest(app)
+    .get("/concentration?majid=2")
     .expect(200)
     .then((response) => {
-        expect(Array.isArray(response.body)).toBeTruthy();
-        expect(response.body.length).toBeGreaterThan(0);
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toBeGreaterThan(0);
     });
 });
 
-it('test /courses/major endpoint', async() => {
-    await supertest(app).get("courses/major?majid=2")
+it("test /courses/major endpoint", async () => {
+  await supertest(app)
+    .get("/courses/major?majid=2")
     .expect(200)
     .then((response) => {
-        expect(Array.isArray(response.body)).toBeTruthy();
-        expect(response.body.length).toBeGreaterThan(0);
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toBeGreaterThan(0);
     });
 });
 
-it('test /courses/concentration endpoint', async() => {
-    await supertest(app).get("courses/concentration?conid=3")
+it("test /courses/concentration endpoint", async () => {
+  await supertest(app)
+    .get("/courses/concentration?conid=3")
     .expect(200)
     .then((response) => {
-        expect(Array.isArray(response.body)).toBeTruthy();
-        expect(response.body.length).toBeGreaterThan(0);
+      expect(Array.isArray(response.body)).toBeTruthy();
+      expect(response.body.length).toBeGreaterThan(0);
     });
 });
