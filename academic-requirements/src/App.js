270d0d8c33dc97d5f2fa7406e9c1bc4acd513e85
RaffelNicholas <raffeln6264@my.uwstout.edu>
2022-11-15 18:03:25 -0600

    Fixed spelling...

--- academic-requirements/src/App.js
+++ academic-requirements/src/App.js
@@ -177,7 +177,7 @@ function App() {
         showing={clickedGenerate}
         concentrationCourseList={concentrationCourseData}
         majorCourseList={majorCourseData}
-        selectMajor={major}
+        selectdMajor={major}
         selectedConcentration={concentration}
         completedCourses={coursesTaken}
       />
