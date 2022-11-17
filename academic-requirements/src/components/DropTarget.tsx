b158b5da1b8c987425fe9f1f8db49c823209633c
Tyler Pamperin <pamperint8644@my.uwstout.edu>
2022-11-17 17:49:17 -0600

    Algorithm created for dragging in and out a course. Need to test dragging out a course a bit more yet, since that does not fully work as intended

--- academic-requirements/src/components/DropTarget.tsx
+++ academic-requirements/src/components/DropTarget.tsx
@@ -1,5 +1,5 @@
 import update from 'immutability-helper'
-import { FC, useEffect } from 'react'
+import { createContext, FC, useEffect } from 'react'
 import { memo, useCallback, useState } from 'react'
 //@ts-ignore
 import { Course } from './DraggableCourse.tsx'
@@ -11,6 +11,7 @@ import {CourseList} from "./CourseList.tsx"
 import StringProcessing from '../stringProcessing/StringProcessing.tsx';
 import { ItemTypes } from './Constants'
 import React from 'react'
+import { number } from 'prop-types'
 
 interface SemesterState {
   accepts: string[]
@@ -112,14 +113,7 @@ export const Container: FC<ContainerProps> = memo(function Container({
       const prereqCheck = new StringProcessing();
 
       // Get all courses in previous semesters
-      const previousCourses = new Array<string>();
-      semesters.forEach((currSemester) => {
-        if (currSemester.number - 1 < index) {
-          currSemester.courses.forEach((x) => {
-            previousCourses.push(x.subject + '-' + x.number);
-          })
-        }
-      })
+      const previousCourses = getPreviousSemesterCourses(index);
 
       // Get all courses in current semester (excluding the course to be added)
       const currentCourses = new Array<string>();
@@ -127,12 +121,8 @@ export const Container: FC<ContainerProps> = memo(function Container({
         currentCourses.push(x.subject + '-' + x.number);
       })
 
-      console.log('Courses in previous semesters: ' + previousCourses);
-      console.log('Courses in current semester: ' + currentCourses);
-
       // Run the prerequisite check on the course
       if (course) {
-        console.log('prereq string is: ' + course.preReq);
         if (prereqCheck.courseInListCheck(course.preReq, previousCourses, currentCourses)) {
           // prereqCheck returned true, so add the course to the semester
           setSemesters(
@@ -163,13 +153,135 @@ export const Container: FC<ContainerProps> = memo(function Container({
       const {name}=item
       const found = droppedCourses.find(course => course.name === name)
       setDroppedCourses(courses.filter(item => item.name !== name));
-      console.log(JSON.stringify(found))
-      setCourses(
-        update(courses, found ? { $push: [found] } : { $push: [] }),
-      )
+
+      // Find the course's semester before moving it
+      let courseSemesterIndex = -1;
+      if (found) {
+        semesters.forEach((sem, index) => {
+          sem.courses.forEach((c) => {
+            if ('' + c.subject + c.number === '' + found.subject + found.number) {
+              courseSemesterIndex = index;
+            }
+          })
+        })
+
+      // If all courses pass the preReq check, then update the course lists
+      if (preReqCheckCoursesInSemesterAndBeyond(found, courseSemesterIndex)) {
+        setCourses(
+          update(courses, found ? { $push: [found] } : { $push: [] }),
+        )
+
+        // Update semesters to have the course removed
+        // TODO TEST THIS TO MAKE SURE IT WORKS AS INTENDED
+        let itemArr = new Array<CourseState>();
+        if (found) {
+          semesters[courseSemesterIndex].courses.forEach((x) => {
+            if (x.name !== found.name) {
+              itemArr.push(x);
+            }
+          })
+        }
+        setSemesters(
+          update(semesters, {
+            [courseSemesterIndex]: {
+              courses: {
+                $set: itemArr
+              }
+            },
+          }),
+        )
+      }
+      else {
+        // fails to satisfy prerequisites
+        console.log('CANNOT MOVE COURSE! FAILS PREREQUISITES');
+      }
+    }
+    else {
+      //fails to find course...for some reason
+      console.log(`this is bad and shouldn't happen`);
+    }
     },
     [courses],
   )
+
+  // This function checks if every course passes the prerequisite check when moving a course
+  // out of a semester and into the course bank
+  function preReqCheckCoursesInSemesterAndBeyond(courseToRemove: CourseState, courseSemesterIndex: number): boolean {
+
+    // prereqCheck will be used to check prerequisites
+    const preReqCheck = new StringProcessing();
+
+    // Get the course names in the previous semesters
+    const previousCourses = getPreviousSemesterCourses(courseSemesterIndex);
+
+    // Get the current courses in the current semester
+    let currentCourses = getSemesterCourses(courseSemesterIndex);
+    let currentCoursesNames = getSemesterCoursesNames(courseSemesterIndex);
+
+    let preReqsSatisfied = true;
+
+    semesters.forEach((currSemester, index) => {
+      if (preReqsSatisfied && currSemester.number - 1 >= courseSemesterIndex) {
+        // Check every course in the current semester passes the prerequsites
+        currentCourses.forEach((x) => {
+          preReqsSatisfied = preReqsSatisfied && preReqCheck.courseInListCheck(x.preReq, previousCourses, currentCoursesNames)
+        })
+
+        // Append the current semester to the previous courses semester
+        currentCoursesNames.forEach((x) => {
+          // Additional check to ensure the removed course is not included in the course list
+          if (x !== courseToRemove.subject + '-' + courseToRemove.number) {
+            previousCourses.push(x);
+          }
+        })
+        
+        // Update the current course lists to be for the next semester
+        if (index + 1 <= semesters.length) {
+          currentCourses = getSemesterCourses(index + 1);
+          currentCoursesNames = getSemesterCoursesNames(index + 1);
+        }
+      }
+    })
+
+    return preReqsSatisfied;
+  }
+
+  // Get all courses in previous semesters
+  // param semesterIndex -> current semester index
+  function getPreviousSemesterCourses(semesterIndex: number): Array<string> {
+    let previousCourses = new Array();
+    semesters.forEach((currSemester) => {
+      if (currSemester.number - 1 < semesterIndex) {
+        currSemester.courses.forEach((x) => {
+          previousCourses.push(x.subject + '-' + x.number);
+        })
+      }
+    })
+
+    return previousCourses;
+  }
+
+  // Get all CourseState objects in current semester
+  // param semesterIndex -> current semester index
+  function getSemesterCourses(semesterIndex: number): Array<CourseState> {
+    let semCourses = new Array();
+    semesters[semesterIndex].courses.forEach((x) => {
+        semCourses.push(x);
+    })
+
+    return semCourses;
+  }
+
+  // Get all courses (string) in current semester
+  // param semesterIndex -> current semester index
+  function getSemesterCoursesNames(semesterIndex: number): Array<string> {
+    let semCourses = new Array();
+    semesters[semesterIndex].courses.forEach((x) => {
+          semCourses.push(x.subject + '-' + x.number);
+    })
+
+    return semCourses;
+  }
 
   return (
     <div>
