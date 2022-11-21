e077c680f4bda17c8d746bb837f72258f263f5f2
Samuel Hovland <84932449+Hovland-426@users.noreply.github.com>
2022-11-21 13:13:33 -0600

    Added concentrationCourseList to function call.

--- academic-requirements/src/components/FourYearPlanPage.tsx
+++ academic-requirements/src/components/FourYearPlanPage.tsx
@@ -71,7 +71,7 @@ const FourYearPlanPage = (props: {
             error={error}
           />
           <div className="page">
-            <Example PassedCourseList={props.majorCourseList} />
+            <Example PassedCourseList={props.majorCourseList.concat(props.concentrationCourseList)} />
             <div className="right-side">
               <div className="requirements">Requirements</div>
               <button data-testid="ExportButton" onClick={exportSchedule}>
